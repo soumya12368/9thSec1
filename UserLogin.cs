a3366c082dddff7ddebaa7a79341e6802931937a
Soumya Sutar <soumsutar9@gmail.com>
2024-01-09 20:58:29 +0530

    Soumya

---
+++ UserLogin.cs
@@ -0,0 +1,18 @@
+﻿namespace UsersLoginLib
+{
+    public class UserLogin
+    {
+        public string Login(string userName, string userPwd)
+        {
+            string result = "";
+            if(userName == "sam1256" && userPwd == "sam@1256@1256")
+            {
+                result = "Login success";
+            } else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+    }
+}
